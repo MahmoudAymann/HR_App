f9db95758f0e819a37096721e629c063b9426086
Mahmoud Ayman <mahmoud_aymann@outlook.com>
2021-04-02 01:06:06 +0200

    all

--- app/src/main/java/com/gsgroup/hrapp/ui/fragment/news/details/NewsDetailsFragment.kt
+++ app/src/main/java/com/gsgroup/hrapp/ui/fragment/news/details/NewsDetailsFragment.kt
@@ -1,14 +1,14 @@
-package com.gsgroup.hrapp.ui.fragment.news
+package com.gsgroup.hrapp.ui.fragment.news.details
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.gsgroup.hrapp.base.BaseFragment
-import com.gsgroup.hrapp.databinding.FragmentNewsBinding
+import com.gsgroup.hrapp.databinding.FragmentNewsDetailsBinding
 
-class NewsFragment : BaseFragment<FragmentNewsBinding, NewsViewModel>() {
+class NewsDetailsFragment : BaseFragment<FragmentNewsDetailsBinding, NewsDetailsViewModel>() {
     override fun pageTitle(): String = ""
-    override val mViewModel: NewsViewModel by viewModels()
+    override val mViewModel: NewsDetailsViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/gsgroup/hrapp/ui/fragment/news/details/NewsDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.gsgroup.hrapp.ui.fragment.news.details
+
+import com.gsgroup.hrapp.base.BaseViewModel
+
+class NewsDetailsViewModel : BaseViewModel() {
+
+}
